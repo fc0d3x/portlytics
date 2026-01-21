a05d29e7f347cf15c3e40256533752193ffabd29
fc0d3x <filipjovanovv@proton.me>
2026-01-22 00:10:44 +0200

    Update test_portlytics.py

--- test_portlytics.py
+++ test_portlytics.py
@@ -3,8 +3,10 @@ from portlytics import scan_port
 
 def test_scan_port_open():
     with patch("socket.socket.connect_ex", return_value=0):
-        assert scan_port("127.0.0.1", 80) is True
+        # Adding timeout and delay arguments
+        assert scan_port("127.0.0.1", 80, timeout=1.0, delay=None) is True
 
 def test_scan_port_closed():
     with patch("socket.socket.connect_ex", return_value=1):
-        assert scan_port("127.0.0.1", 80) is False
+        # Adding timeout and delay arguments
+        assert scan_port("127.0.0.1", 80, timeout=1.0, delay=None) is False
